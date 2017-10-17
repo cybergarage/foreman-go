bb26e975d81da111adc504a7a99b869079ca1898
Satoshi Konno <skonno@cybergarage.org>
2017-10-18 01:09:13 +0900

    Added initial codes for QoS.

---
+++ foreman/qos/clause_test.go
@@ -0,0 +1,12 @@
+// Copyright (C) 2017 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+package qos
+
+import (
+	"testing"
+)
+
+func TestNewClause(t *testing.T) {
+	NewClause()
+}
---
+++ foreman/qos/rule_test.go
@@ -0,0 +1,13 @@
+// Copyright (C) 2017 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package usat
+
+import (
+	"testing"
+)
+
+func TestNewProblem(t *testing.T) {
+	NewProblem()
+}
