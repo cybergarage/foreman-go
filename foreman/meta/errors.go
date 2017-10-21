58be16c47f15ad0ba3fad3fa6b946d0606902640
Satoshi Konno <skonno@cybergarage.org>
2017-10-21 17:49:08 +0900

    * Added slelton files for meta store.

---
+++ foreman/meta/errors.go
@@ -0,0 +1,11 @@
+// Copyright (C) 2017 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package meta
+
+const (
+	errorClangObjectNotInitialized = "Clang object isn't initialized"
+	errorStoreCouldNotOpen         = "Couldn't open : %s"
+	errorStoreCouldNotClose        = "Couldn't close : %s"
+)
---
+++ foreman/meta/store_test.go
@@ -0,0 +1,13 @@
+// Copyright (C) 2017 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package meta
+
+import (
+	"testing"
+)
+
+func TestNewStore(t *testing.T) {
+	NewStore()
+}
