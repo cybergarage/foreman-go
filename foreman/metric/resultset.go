0193fdcffdc556a47fae3d3cfd5c21e737c4fc15
Satoshi Konno <skonno@cybergarage.org>
2017-10-22 16:59:35 +0900

    * Updated ResultSet to define an interface not to depend on foreman-cc.

--- foreman/metric/resultset.go
+++ foreman/metric/resultset.go
@@ -9,69 +9,9 @@ package metric
 // #cgo LDFLAGS: -lforeman++ -lstdc++
 import "C"
 
-import (
-	"runtime"
-	"unsafe"
-)
-
-// ResultSet represents a Foreman ResultSet.
-type ResultSet struct {
-	cObject unsafe.Pointer
-}
-
-// NewResultSet returns a new ResultSet.
-func NewResultSet() *ResultSet {
-	return NewResultSetWithCObject(C.foreman_metric_resultset_new())
-}
-
-// NewResultSetWithCObject returns a new ResultSet from the C++ object.
-func NewResultSetWithCObject(cObj unsafe.Pointer) *ResultSet {
-	rs := &ResultSet{}
-	rs.cObject = cObj
-	runtime.SetFinalizer(rs, resultSetFinalizer)
-	return rs
-}
-
-func resultSetFinalizer(self *ResultSet) {
-	if self.cObject != nil {
-		if C.foreman_metric_resultset_delete(self.cObject) {
-			self.cObject = nil
-		}
-	}
-}
-
-// GetDataPointCount returns a number of the data points.
-func (self *ResultSet) GetDataPointCount() int {
-	if self.cObject == nil {
-		return 0
-	}
-	return int(C.foreman_metric_resultset_getdatapointcount(self.cObject))
-}
-
-// GetFirstDataPoints returns a first data points.
-func (self *ResultSet) GetFirstDataPoints() *DataPoints {
-	if self.cObject == nil {
-		return nil
-	}
-
-	cDpsObject := C.foreman_metric_resultset_firstdatapoints(self.cObject)
-	if cDpsObject == nil {
-		return nil
-	}
-
-	return NewDataPointsWithCObject(cDpsObject)
-}
-
-// GetNextDataPoints returns a first data points.
-func (self *ResultSet) GetNextDataPoints() *DataPoints {
-	if self.cObject == nil {
-		return nil
-	}
-
-	cDpsObject := C.foreman_metric_resultset_nextdatapoints(self.cObject)
-	if cDpsObject == nil {
-		return nil
-	}
-
-	return NewDataPointsWithCObject(cDpsObject)
+// ResultSet represents an abstract interface of metric store for Foreman.
+type ResultSet interface {
+	GetDataPointCount() int
+	GetFirstDataPoints() *DataPoints
+	GetNextDataPoints() *DataPoints
 }
