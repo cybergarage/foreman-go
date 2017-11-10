89fdebd6d3d965fae583b594905c6fa3447c0049
Satoshi Konno <skonno@cybergarage.org>
2017-11-10 17:26:47 +0900

    * Added skelton file.

---
+++ foreman/errors/error_test.go
@@ -0,0 +1,13 @@
+// Copyright (C) 2017 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package registry provides registry interfaces
+package errors
+
+import (
+	"testing"
+)
+
+func TestNewError(t *testing.T) {
+}
