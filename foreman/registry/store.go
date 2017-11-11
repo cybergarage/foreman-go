cfa2ea2e30faf3023c9c8b1007dbba4a4f86ec98
Satoshi Konno <skonno@cybergarage.org>
2017-11-11 17:52:46 +0900

    * Addded useful methods to registry.Store.

--- foreman/registry/store.go
+++ foreman/registry/store.go
@@ -5,14 +5,26 @@
 // Package registry provides registry interfaces
 package registry
 
-// Store represents an abstract interface of registry.
+import (
+	"strings"
+)
+
+// Storing represents an abstract interface of registry.
 type Storing interface {
 	Open() error
 	Close() error
 	Clear() error
 
+	// To insert a new object using CreateObject, you must set at least the name and parent ID.
+	// You can set the ID explicitly too, but the ID is generated automatically when the ID is not specified.
+	// The generated ID is stored in Object.ID when the object is inserted normally.
+	// The automatic generated ID is guaranteed the uniqueness of the registry store.
 	CreateObject(obj *Object) error
+
+	// To update the inserted object using UpdateObject, you must set the name, data and ID.
+	// UpdateObject can only update the name and data, and it can't update the ID and parent ID.
 	UpdateObject(obj *Object) error
+
 	GetObject(objID string) (*Object, error)
 	DeleteObject(objID string) error
 
@@ -22,14 +34,88 @@ type Storing interface {
 	String() string
 }
 
-// Store represents an abstract interface of registry.
+// Store represents an registry store.
 type Store struct {
 	Storing
 }
 
-// GetRootObjects remove all root objects
-func (self *Store) GetRootObjects() ([]*Object, error) {
+// getStorePathStrings splits the specified path string into an string array.
+func getStorePathStrings(path string) ([]string, error) {
+	allNames := strings.Split(path, PathDelim)
+	names := make([]string, 0)
+	for _, name := range allNames {
+		if len(name) <= 0 {
+			continue
+		}
+		names = append(names, name)
+	}
+	return names, nil
+}
+
+// getStoreObjectByName return a object which has the specified name in a specified object array.
+func getStoreObjectByName(objs []*Object, name string) (*Object, error) {
+	for _, obj := range objs {
+		if obj.IsName(name) {
+			return obj, nil
+		}
+	}
+	return nil, nil
+}
+
+// GetChildObjects returns all child objects.
+func (self *Store) GetChildObjects(parentID string) ([]*Object, error) {
 	q := NewQuery()
-	q.ParentID = RootObjectID
+	q.ParentID = parentID
 	return self.Browse(q)
+}
+
+// GetRootObjects returns all root objects
+func (self *Store) GetRootObjects() ([]*Object, error) {
+	return self.GetChildObjects(RootObjectID)
+}
+
+// GetChildObjectByName returns an object which has the specified name under the specified parent object.
+func (self *Store) GetChildObjectByName(parentID string, name string) (*Object, error) {
+	objs, err := self.GetChildObjects(parentID)
+	if err != nil {
+		return nil, err
+	}
+	return getStoreObjectByName(objs, name)
+}
+
+// GetObjectByNamePath return an object by the specified name path.
+func (self *Store) GetObjectByNamePath(namePath string) (*Object, error) {
+	names, err := getStorePathStrings(namePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var lastObj *Object
+	for n, name := range names {
+		if n == 0 {
+			lastObj, err = self.GetChildObjectByName(RootObjectID, name)
+		} else {
+			lastObj, err = self.GetChildObjectByName(lastObj.ID, name)
+		}
+		if err != nil {
+			return nil, err
+		}
+		if lastObj == nil {
+			return nil, nil
+		}
+	}
+
+	return lastObj, nil
+}
+
+// GetChildObjectsByNamePath return all objects by the specified name path.
+func (self *Store) GetChildObjectsByNamePath(namePath string) ([]*Object, error) {
+	pathObj, err := self.GetObjectByNamePath(namePath)
+	if err != nil {
+		return nil, err
+	}
+	if pathObj == nil {
+		return []*Object{}, nil
+	}
+	return self.GetChildObjects(pathObj.ID)
 }
--- foreman/registry/store_test.go
+++ foreman/registry/store_test.go
@@ -12,28 +12,57 @@ import (
 )
 
 const (
-	testStoreLoopCount = 100
+	testStoreLoopCount         = 100
+	testObjectNameFormat       = "name%d"
+	testObjectDataFormat       = "data%d"
+	testObjectUpdateNameFormat = "new_name%d"
+	testObjectUpdateDataFormat = "new_data%d"
 )
 
-func testCreateRootObjects(t *testing.T, store *Store) {
-	err := store.Clear()
-	if err != nil {
-		t.Error(err)
+func TestPathSpecialSplit(t *testing.T) {
+	specialPaths := []string{"", "/", "/0"}
+	specialPathCounts := []int{0, 0, 1}
+	for n, path := range specialPaths {
+		name, err := getStorePathStrings(path)
+		if err != nil {
+			t.Error(err)
+		}
+		if len(name) != specialPathCounts[n] {
+			t.Errorf("%d != %d", len(name), specialPathCounts[n])
+		}
 	}
+}
 
-	q := NewQuery()
-	q.ParentID = RootObjectID
-	objs, err := store.Browse(q)
-	if len(objs) != 0 {
-		t.Errorf("%d != %d", len(objs), 0)
-		return
-	}
-
-	// Insert
-
+func TestPathSplit(t *testing.T) {
 	for n := 0; n < testStoreLoopCount; n++ {
-		name := fmt.Sprintf("name%d", n)
-		data := fmt.Sprintf("data%d", n)
+		path := ""
+		names := make([]string, n)
+		for i := 0; i < n; i++ {
+			name := fmt.Sprintf("name%d", i)
+			names[i] = name
+			path += fmt.Sprintf("%s%s", PathDelim, name)
+		}
+
+		parseNames, err := getStorePathStrings(path)
+		if err != nil {
+			t.Error(err)
+		}
+		if len(parseNames) != n {
+			t.Errorf("%d != %d", len(parseNames), n)
+		}
+		for i := 0; i < n; i++ {
+			name := fmt.Sprintf("name%d", i)
+			if parseNames[i] != name {
+				t.Errorf("%s != %s", parseNames[i], name)
+			}
+		}
+	}
+}
+
+func ceateRootObjects(t *testing.T, store *Store) {
+	for n := 0; n < testStoreLoopCount; n++ {
+		name := fmt.Sprintf(testObjectNameFormat, n)
+		data := fmt.Sprintf(testObjectDataFormat, n)
 
 		obj := NewObject()
 		obj.ParentID = RootObjectID
@@ -44,12 +73,28 @@ func testCreateRootObjects(t *testing.T, store *Store) {
 			t.Error(err)
 		}
 	}
+}
+
+func testCreateRootObjects(t *testing.T, store *Store) {
+	err := store.Clear()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	objs, err := store.GetRootObjects()
+	if len(objs) != 0 {
+		t.Errorf("%d != %d", len(objs), 0)
+		return
+	}
+
+	// Insert
+
+	ceateRootObjects(t, store)
 
 	// Get
 
-	q = NewQuery()
-	q.ParentID = RootObjectID
-	objs, err = store.Browse(q)
+	objs, err = store.GetRootObjects()
 	if len(objs) != testStoreLoopCount {
 		t.Errorf("%d != %d", len(objs), testStoreLoopCount)
 		return
@@ -68,11 +113,9 @@ func testCreateRootObjects(t *testing.T, store *Store) {
 		}
 	}
 
-	// Updated
+	// Update
 
-	q = NewQuery()
-	q.ParentID = RootObjectID
-	objs, err = store.Browse(q)
+	objs, err = store.GetRootObjects()
 	if len(objs) != testStoreLoopCount {
 		t.Errorf("%d != %d", len(objs), testStoreLoopCount)
 		return
@@ -81,8 +124,8 @@ func testCreateRootObjects(t *testing.T, store *Store) {
 	for n := 0; n < testStoreLoopCount; n++ {
 		obj := objs[n]
 
-		name := fmt.Sprintf("name%d", n)
-		data := fmt.Sprintf("data%d", n)
+		name := fmt.Sprintf(testObjectUpdateNameFormat, n)
+		data := fmt.Sprintf(testObjectUpdateDataFormat, n)
 		obj.Name = name
 		obj.Data = data
 		err := store.UpdateObject(obj)
@@ -102,9 +145,7 @@ func testCreateRootObjects(t *testing.T, store *Store) {
 
 	// Delete
 
-	q = NewQuery()
-	q.ParentID = RootObjectID
-	objs, err = store.Browse(q)
+	objs, err = store.GetRootObjects()
 	if len(objs) != testStoreLoopCount {
 		t.Errorf("%d != %d", len(objs), testStoreLoopCount)
 		return
@@ -117,6 +158,103 @@ func testCreateRootObjects(t *testing.T, store *Store) {
 			t.Errorf("Could not remove [%s]", obj.ID)
 		}
 	}
+
+	objs, err = store.GetRootObjects()
+	if len(objs) != 0 {
+		t.Errorf("%d != %d", len(objs), 0)
+		return
+	}
+}
+
+func createHierarchicalObjects(t *testing.T, store *Store) {
+	parentID := RootObjectID
+	for n := 0; n < testStoreLoopCount; n++ {
+		obj := NewObject()
+		obj.ParentID = parentID
+		obj.Name = fmt.Sprintf(testObjectNameFormat, n)
+		obj.Data = fmt.Sprintf(testObjectDataFormat, n)
+
+		err := store.CreateObject(obj)
+		if err != nil {
+			t.Error(err)
+		}
+
+		parentID = obj.ID
+	}
+}
+
+func testHierarchicalObjects(t *testing.T, store *Store) {
+	err := store.Clear()
+	if err != nil {
+		t.Error(err)
+	}
+
+	objs, err := store.GetRootObjects()
+	if len(objs) != 0 {
+		t.Errorf("%d != %d", len(objs), 0)
+		return
+	}
+
+	// Insert
+
+	createHierarchicalObjects(t, store)
+
+	// Get
+
+	parentID := RootObjectID
+	for n := 0; n < testStoreLoopCount; n++ {
+		objs, err := store.GetChildObjects(parentID)
+		if err != nil {
+			t.Error(err)
+		}
+		if len(objs) != 1 {
+			t.Errorf("%d != %d", len(objs), 1)
+		}
+
+		obj := objs[0]
+		objName := fmt.Sprintf(testObjectNameFormat, n)
+		if !obj.IsName(objName) {
+			t.Errorf("%s != %s", obj.Name, objName)
+		}
+
+		parentID = obj.ID
+	}
+
+	// Get (Path)
+
+	path := ""
+	for n := 0; n < testStoreLoopCount; n++ {
+		var objs []*Object
+		var err error
+
+		if n == 0 {
+			objs, err = store.GetRootObjects()
+		} else {
+			objs, err = store.GetChildObjectsByNamePath(path)
+		}
+
+		if err != nil {
+			t.Error(err)
+		}
+		if len(objs) != 1 {
+			t.Errorf("%d != %d", len(objs), 1)
+		}
+
+		obj := objs[0]
+		objName := fmt.Sprintf(testObjectNameFormat, n)
+		if !obj.IsName(objName) {
+			t.Errorf("%s != %s", obj.Name, objName)
+		}
+
+		path += fmt.Sprintf("%s%s", PathDelim, obj.Name)
+	}
+
+	// Clean
+
+	err = store.Clear()
+	if err != nil {
+		t.Error(err)
+	}
 }
 
 func testStore(t *testing.T, store *Store) {
@@ -126,6 +264,7 @@ func testStore(t *testing.T, store *Store) {
 	}
 
 	testCreateRootObjects(t, store)
+	testHierarchicalObjects(t, store)
 
 	err = store.Close()
 	if err != nil {
