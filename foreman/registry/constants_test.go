cc09b590949edbca2b364c32ff2e3adfdbb0dffe
Satoshi Konno <skonno@cybergarage.org>
2017-11-16 18:32:11 +0900

    * Added constants_test.go.

---
+++ foreman/registry/constants_test.go
@@ -0,0 +1,10 @@
+// Copyright (C) 2017 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package registry provides registry interfaces
+package registry
+
+const (
+	defaultTestLoopCount = 10
+)
