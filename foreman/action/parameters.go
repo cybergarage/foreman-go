c902dbc6d2bace64ef3907cc1b67046c27dbf935
Satoshi Konno <skonno@cybergarage.org>
2017-12-05 20:36:40 +0900

    * Added initial skelton files for the action manager.

---
+++ foreman/action/parameters.go
@@ -0,0 +1,14 @@
+// Copyright (C) 2017 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package action
+
+// Parameters represents a parameter map.
+type Parameters map[string]Parameter
+
+// NewParameters returns a null parameter map.
+func NewParameters() Parameters {
+	m := map[string]Parameter{}
+	return m
+}
