ec69271cf8db089ee58c589b8d084b4b617f87a8
Satoshi Konno <skonno@cybergarage.org>
2018-01-11 17:16:20 +0900

    Added KnowledgeBase::AddRule() and AddRules().

--- foreman/kb/knowledgebase.go
+++ foreman/kb/knowledgebase.go
@@ -4,17 +4,59 @@
 
 package kb
 
+import (
+	"fmt"
+)
+
 //KnowledgeBase includes all knowledge rules.
 type KnowledgeBase struct {
-	Rules     []Rule
-	variables map[string]Variable
+	Rules     []*Rule
+	Variables map[string]Variable
 }
 
 // NewKnowledgeBase returns a new knowledge base instance.
 func NewKnowledgeBase() *KnowledgeBase {
 	kb := &KnowledgeBase{
-		Rules:     make([]Rule, 0),
-		variables: make(map[string]Variable),
+		Rules:     make([]*Rule, 0),
+		Variables: make(map[string]Variable),
 	}
 	return kb
+}
+
+// AddRule adds a new rules.
+func (kb *KnowledgeBase) AddRule(rule *Rule) error {
+	// Add all variables in the rule
+	for _, clause := range rule.Clauses {
+		for _, quality := range clause.Qualities {
+			variable := quality.Variable
+			variableName := variable.GetName()
+
+			mapVariable, ok := kb.Variables[variableName]
+			if !ok {
+				kb.Variables[variableName] = variable
+				continue
+			}
+
+			// Check whether these are a same instance
+			if variable != mapVariable {
+				return fmt.Errorf(errorInvalidRuleVariable, variableName, variable, mapVariable)
+			}
+		}
+	}
+
+	kb.Rules = append(kb.Rules, rule)
+
+	return nil
+}
+
+// AddRules adds a new rules.
+func (kb *KnowledgeBase) AddRules(rules []*Rule) error {
+	for _, rule := range rules {
+		err := kb.AddRule(rule)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
