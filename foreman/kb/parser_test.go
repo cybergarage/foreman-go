3ff2715c967b730301686033851d9c00de75adeb
Satoshi Konno <skonno@cybergarage.org>
2018-01-12 15:11:21 +0900

    Deleted deprecated tests.

--- foreman/kb/parser_test.go
+++ foreman/kb/parser_test.go
@@ -7,38 +7,6 @@ import (
 	"testing"
 )
 
-type TestParser struct {
-	Factory
-	variables map[string]Variable
-}
-
-func (parser *TestParser) CreateVariable(variable interface{}) (Variable, error) {
-	varStr := variable.(string)
-	v, ok := parser.variables[varStr]
-	if ok {
-		return v, nil
-	}
-	parser.variables[varStr] = newTestVariableWithName(varStr)
-	return parser.variables[varStr], nil
-}
-
-func (parser *TestParser) CreateObjective(object interface{}) (Objective, error) {
-	objStr := object.(string)
-	return newTestObjectiveWithString(objStr), nil
-}
-
-func (parser *TestParser) CreateOperator(operator interface{}) (Operator, error) {
-	opeStr := operator.(string)
-	return newTestOperatorWithString(opeStr), nil
-}
-
-func newTestParser() *TestParser {
-	parser := &TestParser{
-		variables: make(map[string]Variable),
-	}
-	return parser
-}
-
 func TestNewParser(t *testing.T) {
-	newTestParser()
+	NewParser()
 }
