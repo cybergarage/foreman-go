68dafce8b17f6941f62f0c0c2f3a39b854e8db0c
Satoshi Konno <skonno@cybergarage.org>
2018-01-25 14:35:05 +0900

    Chaned Clause as a interface instead of struct.

--- foreman/kb/clause.go
+++ foreman/kb/clause.go
@@ -4,45 +4,9 @@
 
 package kb
 
-import "bytes"
-
-// A Clause represents a clause in a QoS rule.
-type Clause struct {
-	Formulas []Formula
-}
-
-// NewClause returns a new clause.
-func NewClause() *Clause {
-	clause := &Clause{
-		Formulas: make([]Formula, 0),
-	}
-	return clause
-}
-
-// AddFormula adds a new formula.
-func (clause *Clause) AddFormula(formula Formula) error {
-	clause.Formulas = append(clause.Formulas, formula)
-	return nil
-}
-
-// String returns a string description of the instance
-func (clause *Clause) String() string {
-	if len(clause.Formulas) == 0 {
-		return ""
-	}
-
-	if len(clause.Formulas) == 1 {
-		return clause.Formulas[0].String()
-	}
-
-	var b bytes.Buffer
-	b.Write([]byte(StartBracket))
-	for n, formula := range clause.Formulas {
-		if 0 < n {
-			b.Write([]byte(SpaceSeparator + FormulaSeparator + SpaceSeparator))
-		}
-		b.Write([]byte(formula.String()))
-	}
-	b.Write([]byte(EndBracket))
-	return b.String()
+// Clause represents a clause interface.
+type Clause interface {
+	AddFormula(formula Formula) error
+	GetFormulas() []Formula
+	String() string
 }
