b7b1d1c198270c0fa99c755e7fadf096f5143c75
Satoshi Konno <skonno@cybergarage.org>
2018-01-25 14:52:54 +0900

    Changed Rule as a interface instead of struct.

--- foreman/kb/rule.go
+++ foreman/kb/rule.go
@@ -4,90 +4,10 @@
 
 package kb
 
-import (
-	"bytes"
-	"strings"
-)
-
-// Rule represents a QoS rule.
-type Rule struct {
-	Clauses []Clause
-}
-
-// NewRule returns a new null rule.
-func NewRule() *Rule {
-	p := &Rule{
-		Clauses: make([]Clause, 0),
-	}
-	return p
-}
-
-// AddClause adds a new clause.
-func (rule *Rule) AddClause(clause Clause) error {
-	rule.Clauses = append(rule.Clauses, clause)
-	return nil
-}
-
-// ParseString parses a specified rule string.
-func (rule *Rule) ParseString(factory Factory, ruleString string) error {
-	clausesString := strings.Split(ruleString, ClauseSeparator)
-
-	for _, clauseString := range clausesString {
-		clause, err := factory.CreateClause(clausesString)
-		if err != nil {
-			return nil
-		}
-		clauseString = strings.Trim(clauseString, (StartBracket + EndBracket))
-		formulasString := strings.Split(clauseString, FormulaSeparator)
-		for _, formulaString := range formulasString {
-			formula, err := factory.CreateFormula(formulasString)
-			if err != nil {
-				return err
-			}
-			formulaString = strings.TrimSpace(formulaString)
-			formulaString = strings.TrimLeft(formulaString, StartBracket)
-			formulaString = strings.TrimRight(formulaString, EndBracket)
-			err = formula.ParseString(factory, formulaString)
-			if err != nil {
-				return err
-			}
-			err = clause.AddFormula(formula)
-			if err != nil {
-				return err
-			}
-		}
-
-		if len(clause.GetFormulas()) <= 0 {
-			continue
-		}
-
-		err = rule.AddClause(clause)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-// String returns a string description of the instance
-func (rule *Rule) String() string {
-	if len(rule.Clauses) == 0 {
-		return ""
-	}
-
-	if len(rule.Clauses) == 1 {
-		return rule.Clauses[0].String()
-	}
-
-	var b bytes.Buffer
-	b.Write([]byte(StartBracket))
-	for n, clause := range rule.Clauses {
-		if 0 < n {
-			b.Write([]byte(SpaceSeparator + ClauseSeparator + SpaceSeparator))
-		}
-		b.Write([]byte(clause.String()))
-	}
-	b.Write([]byte(EndBracket))
-	return b.String()
+// Rule represents a interface.
+type Rule interface {
+	AddClause(clause Clause) error
+	GetClauses() []Clause
+	ParseString(factory Factory, ruleString string) error
+	String() string
 }
