6e2c69643ce8e99123ad69603d693c41d20ebf0e
Satoshi Konno <skonno@cybergarage.org>
2018-02-02 18:39:17 +0900

    Moved HTTP listerer functions to server_http_listner.go.

--- foreman/server.go
+++ foreman/server.go
@@ -6,7 +6,6 @@
 package foreman
 
 import (
-	"net/http"
 	"os"
 
 	"github.com/cybergarage/go-graphite/net/graphite"
@@ -142,25 +141,4 @@ func (server *Server) Restart() error {
 	}
 
 	return nil
-}
-
-// HTTPRequestReceived is a listener for FQL
-func (server *Server) HTTPRequestReceived(r *http.Request, w http.ResponseWriter) {
-	switch r.URL.Path {
-	case serverFQLPath:
-		server.fqlRequestReceived(r, w)
-		return
-	}
-
-	http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-}
-
-// fqlRequestReceived handles FQL requests
-func (server *Server) fqlRequestReceived(r *http.Request, w http.ResponseWriter) {
-	server.badRequestReceived(r, w)
-}
-
-// badRequestReceived returns http.StatusBadRequest
-func (server *Server) badRequestReceived(r *http.Request, w http.ResponseWriter) {
-	http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 }
