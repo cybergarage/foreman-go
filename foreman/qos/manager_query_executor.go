a3041650be46ba2b357aaace74f0b3444497cda7
Satoshi Konno <skonno@cybergarage.org>
2018-02-04 20:25:11 +0900

    Updated ExecuteQuery() to return errors.Error.

--- foreman/qos/manager_query_executor.go
+++ foreman/qos/manager_query_executor.go
@@ -5,12 +5,11 @@
 package qos
 
 import (
-	"fmt"
-
+	"github.com/cybergarage/foreman-go/foreman/errors"
 	"github.com/cybergarage/foreman-go/foreman/fql"
 )
 
 // ExecuteQuery must return the result as a standard array or map.
-func (mgr *Manager) ExecuteQuery(q fql.Query) (interface{}, error) {
-	return nil, fmt.Errorf("")
+func (mgr *Manager) ExecuteQuery(q fql.Query) (interface{}, *errors.Error) {
+	return nil, errors.NewError()
 }
