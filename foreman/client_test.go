93d3169550ee62d376074a9cef74338db9c21aba
Satoshi Konno <skonno@cybergarage.org>
2018-02-06 11:38:44 +0900

    Updated to check the JSON responses.

--- foreman/client_test.go
+++ foreman/client_test.go
@@ -6,21 +6,11 @@ package foreman
 
 import (
 	"testing"
+
+	"github.com/cybergarage/foreman-go/foreman/rpc/json"
 )
 
-func testClientDial(t *testing.T, client *Client) {
-	server := NewServer()
-
-	err := server.Start()
-	if err != nil {
-		t.Error(err)
-	}
-
-	_, err = server.GetHostname()
-	if err != nil {
-		t.Error(err)
-	}
-
+func testClientQueries(t *testing.T, server *Server, client *Client) {
 	testQueries := []string{
 		"EXPORT%20CONFIG",
 	}
@@ -31,14 +21,48 @@ func testClientDial(t *testing.T, client *Client) {
 			t.Error(err)
 		}
 	}
+}
+
+func testClientConfigQuery(t *testing.T, server *Server, client *Client) {
+	configObj, err := client.PostQuery("EXPORT%20CONFIG")
+	if err != nil {
+		t.Error(err)
+	}
+
+	configs := map[string]string{
+		ConfigProductKey: ProductName,
+		ConfigVersionKey: Version,
+	}
+
+	path := json.NewPathWithObject(configObj)
+	for key, value := range configs {
+		configValue, err := path.GetPathString(key)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+		if configValue != value {
+			t.Errorf("[%s] %s != %s", key, configValue, value)
+		}
+	}
+
+}
+
+func TestClientQueries(t *testing.T) {
+	server := NewServer()
+
+	err := server.Start()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	client := NewClient()
+	testClientQueries(t, server, client)
+	testClientConfigQuery(t, server, client)
 
 	err = server.Stop()
 	if err != nil {
 		t.Error(err)
 	}
-}
-
-func TestClientQueries(t *testing.T) {
-	client := NewClient()
-	testClientDial(t, client)
 }
