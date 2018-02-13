994668741040a374c82c550aa4bbc9fddb08b8a3
Satoshi Konno <skonno@cybergarage.org>
2018-02-13 21:32:31 +0900

    Added route_*.

--- foreman/action/constants.go
+++ foreman/action/constants.go
@@ -5,12 +5,16 @@
 package action
 
 const (
-	ActionEncodingBase64 = "base64"
-	ActionLanguagePython = "python"
-	ActionLanguageLua    = "lua"
-	ActionColumnMethods  = "methods"
-	ActionColumnName     = "name"
-	ActionColumnCode     = "code"
-	ActionColumnEncoding = "encoding"
-	ActionColumnLanguage = "language"
+	ActionEncodingBase64   = "base64"
+	ActionLanguagePython   = "python"
+	ActionLanguageLua      = "lua"
+	ActionColumnMethods    = "methods"
+	ActionColumnName       = "name"
+	ActionColumnCode       = "code"
+	ActionColumnEncoding   = "encoding"
+	ActionColumnLanguage   = "language"
+	RouteColumnRoutes      = "routes"
+	RouteColumnName        = "name"
+	RouteColumnSource      = "src"
+	RouteColumnDestination = "dest"
 )
