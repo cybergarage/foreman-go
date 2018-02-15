fcfbc50e6c161c0729b406bb8780536db99485a1
Satoshi Konno <skonno@cybergarage.org>
2018-02-15 21:33:49 +0900

    Moved test scenario files to ../../test.

--- foreman/test/query_scenario_test.go
+++ foreman/test/query_scenario_test.go
@@ -8,16 +8,20 @@ import (
 	"testing"
 )
 
+const (
+	scenarioDirectory = "../../test/"
+)
+
 func TestQueryScenario(t *testing.T) {
 	scenarioFiles := []string{
-		"data/scenario_config_01.csv",
-		"data/scenario_metrics_01.csv",
+		"scenario_config_01.csv",
+		"scenario_metrics_01.csv",
 	}
 
 	s := NewQueryScenario()
 
 	for _, file := range scenarioFiles {
-		err := s.ExecuteFile(file)
+		err := s.ExecuteFile(scenarioDirectory + file)
 		if err != nil {
 			t.Error(err)
 			return
