852fd1ee4162b628bded02e2bd1e7b8662ec0767
Satoshi Konno <skonno@cybergarage.org>
2018-02-16 18:55:04 +0900

    Updated to update only the target variable in QoS.

--- foreman/qos/metric.go
+++ foreman/qos/metric.go
@@ -6,38 +6,25 @@ package qos
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/cybergarage/foreman-go/foreman/kb"
-	"github.com/cybergarage/foreman-go/foreman/metric"
 )
 
 // Metric represents an metric instance for Knowledge base.
 type Metric struct {
 	kb.Variable
-	Name   string
-	entity *metric.Metric
+	Name  string
+	Value float64
 }
 
 // NewMetricWithName returns a new metric instance with the specified name.
 func NewMetricWithName(name string) *Metric {
 	m := &Metric{
-		Name:   name,
-		entity: nil,
+		Name:  name,
+		Value: 0.0,
 	}
 	return m
-}
-
-// HasEntity returns whether the metirc has the entity
-func (m *Metric) HasEntity() bool {
-	if m.entity == nil {
-		return false
-	}
-	return true
-}
-
-// SetEntity sets a entity for the QoS metric.
-func (m *Metric) SetEntity(entity *metric.Metric) {
-	m.entity = entity
 }
 
 // GetName is an interface method of kb.Variable
@@ -45,10 +32,38 @@ func (m *Metric) GetName() string {
 	return m.Name
 }
 
+// SetValue is an interface method of kb.Variable
+func (m *Metric) SetValue(obj interface{}) error {
+	switch obj.(type) {
+	case float64:
+		value, _ := obj.(float64)
+		m.Value = value
+	case float32:
+		value, _ := obj.(float32)
+		m.Value = float64(value)
+	case int:
+		value, _ := obj.(int)
+		m.Value = float64(value)
+	case int32:
+		value, _ := obj.(int32)
+		m.Value = float64(value)
+	case int64:
+		value, _ := obj.(int64)
+		m.Value = float64(value)
+	case string:
+		str, _ := obj.(string)
+		value, err := strconv.ParseFloat(str, 64)
+		if err != nil {
+			return err
+		}
+		m.Value = value
+	default:
+		return fmt.Errorf(errorInvalidVariable, obj)
+	}
+	return nil
+}
+
 // GetValue is an interface method of kb.Variable
 func (m *Metric) GetValue() (interface{}, error) {
-	if m.entity == nil {
-		return nil, fmt.Errorf(errorNullMetricEntity, m.Name)
-	}
-	return m.entity.Value, nil
+	return m.Value, nil
 }
--- foreman/server_metric_listener.go
+++ foreman/server_metric_listener.go
@@ -9,28 +9,35 @@ import (
 	"github.com/cybergarage/foreman-go/foreman/qos"
 )
 
-// RegisterMetricAdded is a listener for metric.Register
-func (server *Server) RegisterMetricAdded(rm *metric.RegisterMetric) {
-	metricName, err := rm.GetName()
+// updateQoSMetric updates a metric in QoSs
+func (server *Server) updateQoSMetricData(rm *metric.RegisterMetric) {
+	name, err := rm.GetName()
+	if err != nil {
+		return
+	}
+
+	ver, ok := server.qosMgr.GetVariable(name)
+	if !ok {
+		return
+	}
+
+	value, err := rm.GetValue()
+	if err != nil {
+		return
+	}
+
+	ver.SetValue(value)
+}
+
+// updateRegisterMetricListeners updates all listeners
+func (server *Server) updateRegisterMetricListeners(rm *metric.RegisterMetric) {
+	name, err := rm.GetName()
 	if err != nil {
 		return
 	}
 
 	q := qos.NewQuery()
-	q.Target = metricName
-
-	// Set a new metric into the related formulas
-
-	formulas, err := server.qosMgr.FindRelatedFormulas(q)
-	if err != nil {
-		return
-	}
-
-	for _, formula := range formulas {
-		formula.SetMetricEntity(rm.Metric)
-	}
-
-	// Set a new metric into the related rules
+	q.Target = name
 
 	rules, err := server.qosMgr.FindRelatedRules(q)
 	if err != nil {
@@ -43,7 +50,15 @@ func (server *Server) RegisterMetricAdded(rm *metric.RegisterMetric) {
 	}
 }
 
+// RegisterMetricAdded is a listener for metric.Register
+func (server *Server) RegisterMetricAdded(rm *metric.RegisterMetric) {
+	// FIXME : Optimize the listener
+	server.updateQoSMetricData(rm)
+	server.updateRegisterMetricListeners(rm)
+}
+
 // RegisterMetricUpdated is a listener for metric.Register
 func (server *Server) RegisterMetricUpdated(rm *metric.RegisterMetric) {
-	// RegisterMetricUpdated does nothing because the metric change is watched by the related rules directly.
+	// FIXME : QoS which is added after the metrics doesn't listen
+	server.updateQoSMetricData(rm)
 }
