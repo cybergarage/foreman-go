0e6614a7b7c2ae18e938b4aaeafbf14898201cca
Satoshi Konno <skonno@cybergarage.org>
2018-02-17 18:16:16 +0900

    fix fetching foreman-cc deb, add foremantest binary

--- util/prep.sh
+++ util/prep.sh
@@ -1,5 +1,10 @@
 #! /bin/sh
-# Assume foreman-cc tags are in sync
-tag=$(git describe --abbrev=0 --tags)
-curl -sLO "https://https://raw.github.com/cybergarage/foreman-cc/releases/download/$tag/foreman-cc_$tag-1_amd64.deb"
-dpkg -i "foreman-cc_$tag-1_amd64.deb"
+
+set -xue
+
+# Get latest foreman-cc package
+cc_project="$(git ls-remote --get-url origin | sed 's/.*ghe.corp.cybergarage.org.//;s/-go\(.git\)\?$/-cc/')"
+github_url="https://ghe.corp.cybergarage.org/api/v3/repos/$cc_project/releases"
+deb_url=$(curl -sL "$github_url" | jq -r '.[0].assets[] | select(.name | contains(".deb")) | .browser_download_url')
+curl -sL "$deb_url" > foreman-cc.deb
+dpkg -i foreman-cc.deb
