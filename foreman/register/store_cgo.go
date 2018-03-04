3acdc38b20ae428b8e5d42ca09f75247ed3beba3
Satoshi Konno <skonno@cybergarage.org>
2018-03-04 13:35:28 +0900

    Updated to publc cgoStore to use from other package such as foreman.metric.

--- foreman/register/store_cgo.go
+++ foreman/register/store_cgo.go
@@ -14,27 +14,32 @@ import (
 // #include <foreman/foreman-c.h>
 import "C"
 
-// cgoStore represents an register store.
-type cgoStore struct {
+// CgoStore represents an register store.
+type CgoStore struct {
 	Store
 	cStore unsafe.Pointer
 }
 
 // newStoreWithCObject returns a new object with the C++ object.
-func newStoreWithCObject(cObject unsafe.Pointer) *cgoStore {
-	store := &cgoStore{
+func newStoreWithCObject(cObject unsafe.Pointer) *CgoStore {
+	store := &CgoStore{
 		cStore: cObject,
 	}
 	return store
 }
 
 // NewStore returns a new base object.
-func NewStore() *cgoStore {
+func NewStore() *CgoStore {
 	return newStoreWithCObject(C.foreman_register_store_new())
 }
 
+// GetCObject returns the internal Clang object.
+func (store *CgoStore) GetCObject() unsafe.Pointer {
+	return store.cStore
+}
+
 // Open opens the store.
-func (store *cgoStore) Open() error {
+func (store *CgoStore) Open() error {
 	if !C.foreman_register_store_open(store.cStore) {
 		return fmt.Errorf(errorInvalidStore, store.String())
 	}
@@ -42,7 +47,7 @@ func (store *cgoStore) Open() error {
 }
 
 // Close closes the store.
-func (store *cgoStore) Close() error {
+func (store *CgoStore) Close() error {
 	if !C.foreman_register_store_close(store.cStore) {
 		return fmt.Errorf(errorInvalidStore, store.String())
 	}
@@ -50,7 +55,7 @@ func (store *cgoStore) Close() error {
 }
 
 // Clear clears all registers.
-func (store *cgoStore) Clear() error {
+func (store *CgoStore) Clear() error {
 	if !C.foreman_register_store_clear(store.cStore) {
 		return fmt.Errorf(errorInvalidStore, store.String())
 	}
@@ -58,7 +63,7 @@ func (store *cgoStore) Clear() error {
 }
 
 // SetObject sets a object into the store.
-func (store *cgoStore) SetObject(obj Object) error {
+func (store *CgoStore) SetObject(obj Object) error {
 	cerr := C.foreman_error_new()
 	defer C.foreman_error_delete(cerr)
 
@@ -70,7 +75,7 @@ func (store *cgoStore) SetObject(obj Object) error {
 }
 
 // GetObject gets the specified object.
-func (store *cgoStore) GetObject(key string) (Object, bool) {
+func (store *CgoStore) GetObject(key string) (Object, bool) {
 	cObj := C.foreman_register_object_new()
 
 	cErr := C.foreman_error_new()
@@ -85,7 +90,7 @@ func (store *cgoStore) GetObject(key string) (Object, bool) {
 }
 
 // RemoveObject removes the specified key object.
-func (store *cgoStore) RemoveObject(key string) bool {
+func (store *CgoStore) RemoveObject(key string) bool {
 	cErr := C.foreman_error_new()
 	defer C.foreman_error_delete(cErr)
 
@@ -97,11 +102,11 @@ func (store *cgoStore) RemoveObject(key string) bool {
 }
 
 // Size returns the object count.
-func (store *cgoStore) Size() int64 {
+func (store *CgoStore) Size() int64 {
 	return int64(C.foreman_register_store_size(store.cStore))
 }
 
 // String returns a string description of the instance
-func (store *cgoStore) String() string {
+func (store *CgoStore) String() string {
 	return fmt.Sprintf("%s/%s", C.GoString(C.foreman_register_store_gettype(store.cStore)), C.GoString(C.foreman_register_store_getversion(store.cStore)))
 }
