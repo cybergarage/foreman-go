5f1bd17497e770e3fb8657b7f8b3640b31822f99
Satoshi Konno <skonno@cybergarage.org>
2018-03-08 20:10:32 +0900

    build foreman on CentOS; add systemd service

--- util/prep.sh
+++ util/prep.sh
@@ -5,9 +5,9 @@ set -xue
 # Get latest foreman-cc package
 cc_project="$(git ls-remote --get-url origin | sed 's/.*ghe.corp.cybergarage.org.//;s/-go\(.git\)\?$/-cc/')"
 github_url="https://ghe.corp.cybergarage.org/api/v3/repos/$cc_project/releases"
-deb_url=$(curl -sL "$github_url" | jq -r '.[0].assets[] | select(.name | contains(".deb")) | .browser_download_url')
-curl -sL "$deb_url" > foreman-cc.deb
-dpkg -i foreman-cc.deb
+rpm_url=$(curl -sL "$github_url" | jq -r '.[0].assets[] | select(.name | contains(".rpm")) | .browser_download_url')
+
+yum install "$rpm_url"
 
 if ! [ -d src/github.com/cybergarage/foreman-go ]
 then
