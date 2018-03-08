595b653c8849faa33c80da0b2bc4c73295885388
Satoshi Konno <skonno@cybergarage.org>
2018-03-08 20:19:07 +0900

    Answer yes to yum

--- util/prep.sh
+++ util/prep.sh
@@ -7,7 +7,7 @@ cc_project="$(git ls-remote --get-url origin | sed 's/.*ghe.corp.cybergarage.org
 github_url="https://ghe.corp.cybergarage.org/api/v3/repos/$cc_project/releases"
 rpm_url=$(curl -sL "$github_url" | jq -r '.[0].assets[] | select(.name | contains(".rpm")) | .browser_download_url')
 
-yum install "$rpm_url"
+yum install "$rpm_url" -y
 
 if ! [ -d src/github.com/cybergarage/foreman-go ]
 then
