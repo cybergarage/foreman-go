ac436d0c06eb5f90e467d470b26ce488e1118d53
Satoshi Konno <skonno@cybergarage.org>
2018-04-30 18:34:32 +0900

    Updated Server::Start() to increment the ports when it can not bind the ports.

--- foreman/federated_test.go
+++ foreman/federated_test.go
@@ -10,20 +10,57 @@ import (
 	"github.com/cybergarage/foreman-go/foreman/discovery"
 )
 
+const (
+	testFederatedNodeCont = 10
+)
+
 func federatedMetricsTest(t *testing.T, client *Client, nodes []Node) {
 }
 
-func setupFederatedNodes(t *testing.T) []discovery.Node {
-	nodes := make([]discovery.Node, 0)
-	return nodes
+func setupFederatedNodes(t *testing.T) []*Server {
+	servers := make([]*Server, testFederatedNodeCont)
+
+	for n := 0; n < testFederatedNodeCont; n++ {
+		server := NewServer()
+		err := server.Start()
+		if err != nil {
+			t.Error(err)
+		}
+		servers[n] = server
+	}
+	return servers
+}
+
+func setupStaticFinderWithServers(t *testing.T, servers []*Server) discovery.Finder {
+	nodes := make([]discovery.Node, len(servers))
+
+	for n, server := range servers {
+		node := discovery.Node(server)
+		if node == nil {
+			continue
+		}
+		nodes[n] = node
+	}
+	return discovery.NewStaticFinderWithNodes(nodes)
+}
+
+func stopFederatedNodes(t *testing.T, servers []*Server) {
+	for _, server := range servers {
+		err := server.Stop()
+		if err != nil {
+			t.Error(err)
+		}
+	}
 }
 
 func TestFederatedMetricsWithStaticFinder(t *testing.T) {
 	nodes := setupFederatedNodes(t)
-	finder := discovery.NewStaticFinderWithNodes(nodes)
+	finder := setupStaticFinderWithServers(t, nodes)
 
 	client := NewClient()
 	client.AddFinder(finder)
 
 	//federatedMetricsTest(t, client, nodes)
+
+	stopFederatedNodes(t, nodes)
 }
