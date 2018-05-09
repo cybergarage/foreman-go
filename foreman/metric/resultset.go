ca8f74b1e1bb3bc0eedbfebc28ca9eda555cb76c
Satoshi Konno <skonno@cybergarage.org>
2018-05-09 18:19:13 +0900

    Removed ResultSet::NewResultSetWithSize()

--- foreman/metric/resultset.go
+++ foreman/metric/resultset.go
@@ -14,33 +14,41 @@ type ResultSet interface {
 // goResultSet represents a result set.
 type goResultSet struct {
 	ResultSet
-	Metrics     []*Metrics
+	Metrics []*Metrics
+	// For GetMetricsByName() to access by O(1)
+	metricsMap map[string]*Metrics
+	// For GetFirstMetrics() and GetNextMetrics()
 	iteratorPos int
 }
 
 // NewResultSet returns a new result set.
 func NewResultSet() *goResultSet {
-	return NewResultSetWithSize(0)
-}
-
-// NewResultSetWithSize returns a new result set with the specified size.
-func NewResultSetWithSize(size int) *goResultSet {
 	rs := &goResultSet{
-		Metrics:     make([]*Metrics, size),
+		Metrics:     make([]*Metrics, 0),
+		metricsMap:  make(map[string]*Metrics),
 		iteratorPos: 0,
 	}
 	return rs
 }
 
 // AddMetrics adds a new data point.
-func (rs *goResultSet) AddMetrics(dp *Metrics) error {
-	rs.Metrics = append(rs.Metrics, dp)
+func (rs *goResultSet) AddMetrics(ms *Metrics) error {
+	rs.Metrics = append(rs.Metrics, ms)
 	return nil
 }
 
 // GetMetricsCount returns a number of the data points.
 func (rs *goResultSet) GetMetricsCount() int {
 	return len(rs.Metrics)
+}
+
+// GetMetricsByName returns a metrics by name.
+func (rs *goResultSet) GetMetricsByName(name string) *Metrics {
+	ms, ok := rs.metricsMap[name]
+	if !ok {
+		return nil
+	}
+	return ms
 }
 
 // GetFirstMetrics returns a first data points.
