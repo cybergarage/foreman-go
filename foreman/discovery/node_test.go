87bd6444f23f12ac69e9c82bdc7e541760b9c124
Satoshi Konno <skonno@cybergarage.org>
2018-05-09 15:19:40 +0900

    Added NodeEqual()

--- foreman/discovery/node_test.go
+++ foreman/discovery/node_test.go
@@ -34,3 +34,56 @@ func TestNewBaseNode(t *testing.T) {
 	node.Name = testNodeName
 	nodeMachingTest(t, node)
 }
+
+func TestNodeEqual(t *testing.T) {
+	node01 := newBaseNode().(*baseNode)
+
+	node02 := newBaseNode().(*baseNode)
+
+	// name
+
+	node01.Name = "node01"
+	node01.Address = ""
+	node02.Name = "node02"
+	node02.Address = ""
+
+	if !NodeEqual(node01, node01) {
+		t.Errorf("%s != %s", node01.GetName(), node01.GetName())
+	}
+
+	if NodeEqual(node01, node02) {
+		t.Errorf("%s == %s", node01.GetName(), node02.GetName())
+	}
+
+	// address
+
+	node01.Name = ""
+	node01.Address = "192.168.100.1"
+	node02.Name = ""
+	node02.Address = "192.168.100.2"
+
+	if !NodeEqual(node01, node01) {
+		t.Errorf("%s != %s", node01.GetName(), node01.GetName())
+	}
+
+	if NodeEqual(node01, node02) {
+		t.Errorf("%s == %s", node01.GetName(), node02.GetName())
+	}
+
+	// port
+
+	node01.Name = "node01"
+	node01.Address = "192.168.100.1"
+	node01.RPCPort = 0001
+	node02.Name = node01.Name
+	node02.Address = node01.Address
+	node02.RPCPort = 0002
+
+	if !NodeEqual(node01, node01) {
+		t.Errorf("%s != %s", node01.GetName(), node01.GetName())
+	}
+
+	if NodeEqual(node01, node02) {
+		t.Errorf("%s == %s", node01.GetName(), node02.GetName())
+	}
+}
