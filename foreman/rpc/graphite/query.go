eb5ce19a34e50255f4ed5506c37be4de65629cb1
Satoshi Konno <skonno@cybergarage.org>
2018-05-09 20:27:49 +0900

    Added NewMetricQueryWithGraphiteQuery()

--- foreman/rpc/graphite/query.go
+++ foreman/rpc/graphite/query.go
@@ -12,10 +12,17 @@ import (
 // NewGraphiteQueryWithMetricQuery returns a new query from metric.Query.
 func NewGraphiteQueryWithMetricQuery(mq *metric.Query) (*graphite.Query, error) {
 	gq := graphite.NewQuery()
-
 	gq.Target = mq.Target
 	gq.From = mq.From
 	gq.Until = mq.Until
-
 	return gq, nil
+}
+
+// NewMetricQueryWithGraphiteQuery returns a new query from graphite.Query.
+func NewMetricQueryWithGraphiteQuery(gq *graphite.Query) (*metric.Query, error) {
+	mq := metric.NewDataQuery()
+	mq.Target = gq.Target
+	mq.From = gq.From
+	mq.Until = gq.Until
+	return mq, nil
 }
