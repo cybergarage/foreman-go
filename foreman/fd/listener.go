6c82a77a119edc26f5c3ea7608efa782bd2697ea
Satoshi Konno <skonno@cybergarage.org>
2018-07-06 21:26:16 +0900

    Add comments

--- foreman/fd/listener.go
+++ foreman/fd/listener.go
@@ -6,8 +6,12 @@ package fd
 
 // FailureDetectorListener a listener for detector.
 type FailureDetectorListener interface {
+	// FailureDetectorNodeAdded is called when a new node added in the cluster.
 	FailureDetectorNodeAdded(Node)
+	// FailureDetectorNodeRemoved is called when the node is removed in the cluster.
 	FailureDetectorNodeRemoved(Node)
+	// FailureDetectorNodeStatusChanged is called when a status of the node is changed  in the cluster.
 	FailureDetectorNodeStatusChanged(Node)
+	// FailureDetectorNodeOutOfDate is called when the node is out of date in the cluster.
 	FailureDetectorNodeOutOfDate(Node)
 }
