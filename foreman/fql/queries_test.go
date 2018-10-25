480d6ca4964593d626532bf1c0cc29f13b98308c
Satoshi Konno <skonno@cybergarage.org>
2018-10-25 16:00:50 +0900

    Add Queries::Hash() and Equals to compare queries easily

--- foreman/fql/queries_test.go
+++ foreman/fql/queries_test.go
@@ -5,9 +5,54 @@
 package fql
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
+)
+
+const (
+	queryHashTestFilename = loaderTestFilename
 )
 
 func TestNewQueries(t *testing.T) {
 	NewQueries()
+}
+
+func TestNewQueriesHash(t *testing.T) {
+	testDir, _ := os.Getwd()
+	filename := filepath.Join(testDir, queryHashTestFilename)
+
+	loader := NewLoader()
+
+	// Q1
+
+	q1, err := loader.LoadFromFile(filename)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	q1Hash, err := q1.Hash()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	// Q2
+
+	q2, err := loader.LoadFromFile(filename)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	q2Hash, err := q2.Hash()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	// Compare
+
+	if !q1.Equals(q2) {
+		t.Errorf("%s == %s", q1Hash, q2Hash)
+	}
 }
