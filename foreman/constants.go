9945282ec8744c954a1f311cd887e7a8422e7974
Satoshi Konno <skonno@cybergarage.org>
2018-10-27 18:51:41 +0900

    Add default_finder

--- foreman/constants.go
+++ foreman/constants.go
@@ -4,40 +4,41 @@
 
 package foreman
 
-const (
-	ProductName                       = "foreman"
-	DefaultServerHost                 = "localhost"
-	DefaultCarbonPort                 = 2003
-	DefaultHttpPort                   = 8188
-	DefaultRpcProtocol                = "http"
-	DefaultLogFile                    = "-"
-	DefaultLogLevel                   = "INFO"
-	DefaultMetricsStore               = "sqlite"
-	DefaultMetricsInterval            = 300
-	DefaultMetricsPeriod              = 3600
-	DefaultCluster                    = "foreman"
-	DefaultBoostrapMode               = 0
-	HttpRequestFqlPath                = "/fql"
-	HttpRequestFqlQueryParam          = "q"
-	HttpRequestFqlRetransmissionParam = "r"
-	ConfigCategoryKey                 = "config"
-	ConfigProductKey                  = "product"
-	ConfigVersionKey                  = "version"
-	ConfigHostKey                     = "host"
-	ConfigCarbonPortKey               = "carbon_port"
-	ConfigHttpPortKey                 = "http_port"
-	ConfigFqlPathKey                  = "fql_path"
-	ConfigFqlQueryKey                 = "fql_query"
-	ConfigLogFileKey                  = "log_file"
-	ConfigLogLevelKey                 = "log_level"
-	ConfigBoostrapKey                 = "boostrap"
-	ConfigFinderKey                   = "finder"
-	ConfigMetricsStore                = "metrics_store"
-	FinderEchonet                     = "echonet"
-	RpcError                          = "error"
-	RpcErrorCode                      = "code"
-	RpcErrorMessage                   = "message"
-	RpcErrorDetail                    = "detail"
-	RpcErrorDetailCode                = "code"
-	RpcErrorDetailMessage             = "message"
+const ( 
+ProductName = "foreman"
+DefaultServerHost = "localhost"
+DefaultCarbonPort = 2003
+DefaultHttpPort = 8188
+DefaultRpcProtocol = "http"
+DefaultLogFile = "-"
+DefaultLogLevel = "INFO"
+DefaultMetricsStore = "sqlite"
+DefaultMetricsInterval = 300
+DefaultMetricsPeriod = 3600
+DefaultCluster = "foreman"
+DefaultBoostrapMode = 0
+DefaultFinder = "echonet"
+HttpRequestFqlPath = "/fql"
+HttpRequestFqlQueryParam = "q"
+HttpRequestFqlRetransmissionParam = "r"
+ConfigCategoryKey = "config"
+ConfigProductKey = "product"
+ConfigVersionKey = "version"
+ConfigHostKey = "host"
+ConfigCarbonPortKey = "carbon_port"
+ConfigHttpPortKey = "http_port"
+ConfigFqlPathKey = "fql_path"
+ConfigFqlQueryKey = "fql_query"
+ConfigLogFileKey = "log_file"
+ConfigLogLevelKey = "log_level"
+ConfigBoostrapKey = "boostrap"
+ConfigFinderKey = "finder"
+ConfigMetricsStore = "metrics_store"
+FinderEchonet = "echonet"
+RpcError = "error"
+RpcErrorCode = "code"
+RpcErrorMessage = "message"
+RpcErrorDetail = "detail"
+RpcErrorDetailCode = "code"
+RpcErrorDetailMessage = "message"
 )
