146ad67b477cee883290e3aa5b5145ed5c4b285b
Satoshi Konno <skonno@cybergarage.org>
2018-10-27 16:21:58 +0900

    Change Controller to have only a finder

--- foreman/controller.go
+++ foreman/controller.go
@@ -11,102 +11,82 @@ import (
 
 // Controller represents a controller.
 type Controller struct {
-	Finders []discovery.Finder
+	Finder discovery.Finder
 }
 
 // NewController returns a new controller.
 func NewController() *Controller {
 	ctrl := &Controller{
-		Finders: make([]discovery.Finder, 0),
+		Finder: nil,
 	}
 	return ctrl
 }
 
-// AddFinder adds a specified finder.
-func (ctrl *Controller) AddFinder(finder discovery.Finder) error {
-	ctrl.Finders = append(ctrl.Finders, finder)
+// SetFinder sets a specified finder.
+func (ctrl *Controller) SetFinder(finder discovery.Finder) error {
+	ctrl.Finder = finder
 	return nil
 }
 
 // HasFinders returns whether the controller has no finder.
 func (ctrl *Controller) HasFinders() bool {
-	if len(ctrl.Finders) <= 0 {
+	if ctrl.Finder == nil {
 		return false
 	}
 	return true
 }
 
-// SearchAll searches all nodes.
+// Search searches all nodes.
 func (ctrl *Controller) Search() error {
-	for _, finder := range ctrl.Finders {
-		err := finder.Search()
-		if err != nil {
-			return err
-		}
-	}
-	return nil
+	return ctrl.Finder.Search()
 }
 
 // SetSearchListener sets a specified listener.
 func (ctrl *Controller) SetSearchListener(l discovery.FinderSearchListener) error {
-	for _, finder := range ctrl.Finders {
-		err := finder.SetSearchListener(l)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
+	return ctrl.Finder.SetSearchListener(l)
 }
 
 // SetNotifyListener sets a specified listener.
 func (ctrl *Controller) SetNotifyListener(l discovery.FinderNotifyListener) error {
-	for _, finder := range ctrl.Finders {
-		err := finder.SetNotifyListener(l)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
+	return ctrl.Finder.SetNotifyListener(l)
 }
 
 // GetAllNodes returns all found nodes.
 func (ctrl *Controller) GetAllNodes() ([]Node, error) {
-	allNodes := make([]Node, 0)
-	for _, finder := range ctrl.Finders {
-		nodes, err := finder.GetAllNodes()
-		if err != nil {
-			return nil, err
-		}
-		for _, node := range nodes {
-			allNodes = append(allNodes, NewRemoteNodeWithNode(node))
-		}
+	finderNodes, err := ctrl.Finder.GetAllNodes()
+	if err != nil {
+		return nil, err
 	}
+
+	allNodes := make([]Node, 0)
+	for _, finderNode := range finderNodes {
+		allNodes = append(allNodes, NewRemoteNodeWithNode(finderNode))
+	}
+
 	return allNodes, nil
 }
 
 // GetResponsibleNodesForMetric returns a responsible node for a specified metric.
 func (ctrl *Controller) GetResponsibleNodesForMetric(m *metric.Metric) ([]Node, error) {
 	name := m.GetName()
-	respNodes := make([]Node, 0)
-	for _, finder := range ctrl.Finders {
-		nodes, err := finder.GetPrefixNodes(name)
-		if err != nil {
-			return nil, err
-		}
-		for _, node := range nodes {
-			respNodes = append(respNodes, NewRemoteNodeWithNode(node))
-		}
+	finderNodes, err := ctrl.Finder.GetPrefixNodes(name)
+	if err != nil {
+		return nil, err
 	}
+
+	respNodes := make([]Node, 0)
+	for _, finderNode := range finderNodes {
+		respNodes = append(respNodes, NewRemoteNodeWithNode(finderNode))
+	}
+
 	return respNodes, nil
 }
 
 // GetNeighborhoodRemoteNode returns a neighborhood remote node of this node.
 func (ctrl *Controller) GetNeighborhoodRemoteNode(node Node) (*RemoteNode, error) {
-	for _, finder := range ctrl.Finders {
-		node, err := finder.GetNeighborhoodNode(node)
-		if err != nil {
-			return NewRemoteNodeWithNode(node), nil
-		}
+	finderNode, err := ctrl.Finder.GetNeighborhoodNode(node)
+	if err != nil {
+		return nil, err
 	}
-	return nil, nil
+	return NewRemoteNodeWithNode(finderNode), nil
 }
