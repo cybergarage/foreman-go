f8e5250cb598faa8dc89480b055fd43a45daf8ec
Satoshi Konno <skonno@cybergarage.org>
2019-01-16 02:42:45 +0900

    Fix Python codes in scenario_action_py_05

--- test/actions/base64_encode_actions.sh
+++ test/actions/base64_encode_actions.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-ls -1 *.py | xargs -Ifname sh -c 'base64 -w 0 fname > fname.base64'
+# Linux
+#ls -1 *.py | xargs -Ifname sh -c 'base64 -w 0 fname > fname.base64'
+# MacOSX
+ls -1 *.py | xargs -Ifname base64 -b 0 -i fname -o fname.base64
