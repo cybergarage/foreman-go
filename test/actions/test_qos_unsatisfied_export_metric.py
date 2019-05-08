38f1239275f7002f8439e18a83b14455314ddabf
Satoshi Konno <skonno@cybergarage.org>
2019-05-08 19:50:00 +0900

    Add scenario_qos_04 #73

---
+++ test/actions/test_qos_unsatisfied_export_metric.py
@@ -0,0 +1,7 @@
+import foreman
+def qos_unsatisfied_export_metric(params,results):
+    for key, _ in params.iteritems():
+        res_json = foreman.execute_query('SELECT * FROM METRICS WHERE name == %s' % key)
+        metrics_val = res_json[0]['datapoints'][-1]
+        foreman.set_register('%s_metric' % key, str(metrics_val))
+    return True
