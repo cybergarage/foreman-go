4e02a0a220bec7892538da7e233341fee435d7ae
Satoshi Konno <skonno@cybergarage.org>
2019-05-16 13:54:15 +0900

    Delete deprecated actions

--- test/scenarios/actions/qos_action_01.py
+++
@@ -1,8 +0,0 @@
-import foreman
-def qos_action_01(params,results):
-    for key, value in params.items():
-        chkKey = 'qos_action_01_%s' % key
-        foreman.set_register(chkKey, str(value))
-        # Pass all QoS parameters to qos_action_02
-        results[key] = value
-    return True
