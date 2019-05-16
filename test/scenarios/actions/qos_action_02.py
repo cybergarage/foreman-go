56a11a8393240322a8dc11cfabef7cb178187b6d
Satoshi Konno <skonno@cybergarage.org>
2019-05-16 13:54:15 +0900

    Delete deprecated actions

--- test/scenarios/actions/qos_action_02.py
+++
@@ -1,8 +0,0 @@
-import foreman
-def qos_action_02(params,results):
-    for key, value in params.items():
-        chkKey = 'qos_action_02_%s' % key
-        foreman.set_register(chkKey, str(value))
-        results[key] = value
-    # Returns a false not to run the related routes
-    return False
--- test/scenarios/actions/qos_action_03.py
+++
@@ -1,8 +0,0 @@
-import foreman
-# The action (qos_action_03) is not executed because the destination action (qos_action_02) returns a false
-def qos_action_03(params,results):
-    for key, value in params.items():
-        chkKey = 'qos_action_03_%s' % key
-        foreman.set_register(chkKey, str(value))
-        results[key] = value
-    return True
