419ddb90714f9a035cd0d7c90d2f61eeb1b98402
Satoshi Konno <skonno@cybergarage.org>
2019-05-17 17:22:18 +0900

    Support SHELL command to exec any commands easily #73

---
+++ foreman/test/shell_event.go
@@ -0,0 +1,39 @@
+// Copyright (C) 2017 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package test
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+// ShellEvent represents a scenario shell event.
+type ShellEvent struct {
+	Command string
+}
+
+// NewShellEvent create an scenario shell event
+func NewShellEvent() *ShellEvent {
+	q := &ShellEvent{}
+	return q
+}
+
+// ParseEvent parses the specified event.
+func (q *ShellEvent) ParseEvent(e *Event) error {
+	eventData := e.Data
+	if len(eventData) <= 0 {
+		return errors.New(errorEmptyEvent)
+	}
+
+	params := strings.SplitN(eventData, " ", 2)
+	if len(params) != 2 {
+		return fmt.Errorf(errorInvalidEventParameterCount, len(params), e.Data)
+	}
+
+	q.Command = params[1]
+
+	return nil
+}
