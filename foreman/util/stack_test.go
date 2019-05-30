28c8c2d5e0bae5fedb669d133b4ad78c839e9ba2
Satoshi Konno <skonno@cybergarage.org>
2019-05-30 14:40:19 +0900

    Rename fql.ParserObjectStack to util.Stack #77 #72 #62

--- foreman/util/stack_test.go
+++ foreman/util/stack_test.go
@@ -2,19 +2,19 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package fql
+package util
 
 import (
 	"testing"
 )
 
 const (
-	errorInvalidParserObject    = "Invalid parser object (%d != %d)"
+	errorInvalid                = "Invalid parser object (%d != %d)"
 	errorInvalidParserStackSize = "Invalid parser stack size (%d != %d)"
 )
 
-func TestNewParserObjectStack(t *testing.T) {
-	s := NewParserObjectStack()
+func TestNewStack(t *testing.T) {
+	s := NewStack()
 
 	if s.Size() != 0 {
 		t.Errorf(errorInvalidParserStackSize, s.Size(), 0)
@@ -23,17 +23,17 @@ func TestNewParserObjectStack(t *testing.T) {
 	for n := 1; n <= 10; n++ {
 		var value int
 		value = n
-		s.PushObject(value)
+		s.Push(value)
 		if s.Size() != n {
 			t.Errorf(errorInvalidParserStackSize, s.Size(), n)
 		}
 	}
 
 	for n := 10; n >= 1; n-- {
-		obj := s.PopObject()
+		obj := s.Pop()
 		value, _ := obj.(int)
 		if value != n {
-			t.Errorf(errorInvalidParserObject, value, n)
+			t.Errorf(errorInvalid, value, n)
 		}
 		if s.Size() != (n - 1) {
 			t.Errorf(errorInvalidParserStackSize, s.Size(), (n - 1))
