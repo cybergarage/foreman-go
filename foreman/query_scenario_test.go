7b9067b32ff01f28de4e617805346a9f3715a838
Satoshi Konno <skonno@cybergarage.org>
2019-06-17 19:00:02 +0900

    Rename scenario_qos_02.test to scenario_qos_unsatisfied_metrics.test

--- foreman/query_scenario_test.go
+++ foreman/query_scenario_test.go
@@ -49,7 +49,7 @@ func TestQueryBasicScenarios(t *testing.T) {
 		"scenario_finder_export.test",
 		"scenario_metrics_export.test",
 		"scenario_qos_unsatisfied_metric.test",
-		"scenario_qos_02.test",
+		"scenario_qos_unsatisfied_metrics.test",
 		"scenario_qos_03.test",
 		"scenario_qos_05.test",
 		"scenario_register_set.test",
