8d9a5f5690b3d758fc069500cf7ced3f96c788d1
Satoshi Konno <skonno@cybergarage.org>
2019-06-17 18:35:26 +0900

    Rename scenario_action_py_03.test to scenario_action_py_register.test

--- foreman/query_scenario_test.go
+++ foreman/query_scenario_test.go
@@ -77,7 +77,7 @@ func TestQueryPythonActionScenarios(t *testing.T) {
 	scenarioFiles := []string{
 		"scenario_action_py_01.test",
 		"scenario_action_py_02.test",
-		"scenario_action_py_03.test",
+		"scenario_action_py_register.test",
 		"scenario_action_py_04.test",
 		"scenario_action_py_05.test",
 		"scenario_action_py_analyze_metrics.test",
